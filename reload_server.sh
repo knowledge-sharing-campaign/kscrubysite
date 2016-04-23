358238e0d57c6a6cbc3bb3f479406bffc000c826
Dé <rahul080327@gmail.com>
2016-04-23 19:48:40 +0530

    Rahul | Add reload script

--- reload_server.sh
+++ reload_server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo -i
-bundle install
-pkill puma || true
-puma -d -p 80 config.ru || true
-exit
+sudo /usr/local/bin/bundle install
+sudo pkill puma || true
+sudo /usr/local/bin/puma -d -p 80 config.ru || true
