1f788225cc66a1982150e9f6b18c1970cef2a576
Gary <gary.gabrielcmr@gmail.com>
2016-05-06 18:16:20 +0530

    Alvin/Yaalde fix redirect to root_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       login(user.id)
       redirect_to user_path(user)
     else
-      redirect_to new_session_path, alert: "Invalid username or password"
+      redirect_to root_path, alert: "You Successfully signed out"
     end
   end
 
