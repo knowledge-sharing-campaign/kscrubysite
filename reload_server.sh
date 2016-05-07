97f4057b8f3735c8432b6d23397d71f0c21f7b2b
Dé <rahul080327@gmail.com>
2016-05-07 11:15:15 +0530

    Rahul | Fixing Server restarts for Puma and adding rake migrate

--- reload_server.sh
+++ reload_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo /usr/local/bin/bundle install
+sudo /usr/local/bin/rake db:migrate
 sudo pkill puma || true
-sudo /usr/local/bin/puma -d -e production -p 80 config.ru || true
+sudo /usr/local/bin/puma -d -p 80 config.ru || true
