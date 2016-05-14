4d3a4024d9c5f21847fa98614ce594213b519c6c
Abdulsattar Mohammed <asattar.md@gmail.com>
2016-05-14 14:42:08 +0530

    Fix City etc. regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ActiveRecord::Base
 
-
-  attr_accessor :tos
-  validates :tos, presence: true, acceptance: true
+  validates :tos, acceptance: true
 
 
   EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  NAME_REGEX = /\A\[A-Z][a-z][A-Za-z]/
-  USERNAME_REGEX= /\A[a-z0-9-]\z/
-  CITY_REGEX = /\A\[A-Z][a-z][A-Za-z]/
+  NAME_REGEX = /\A[a-zA-Z ']+\z/
+  USERNAME_REGEX= /\A[a-z0-9-]+\z/
+  CITY_REGEX = /\A[a-zA-Z ]+\z/
 
-  validates :first_name, presence: true#, format: { with: NAME_REGEX, message: "can have only characters"}
+  validates :first_name, presence: true, format: { with: NAME_REGEX, message: "can have only characters"}
 
-  validates :last_name, presence: true#, format: { with: NAME_REGEX, message: "can have only characters"}
+  validates :last_name, presence: true, format: { with: NAME_REGEX, message: "can have only characters"}
 
   validates :email, presence: true, format: { with: EMAIL_REGEX, message: "please enter a valid email"}
 
@@ -28,17 +26,17 @@ class User < ActiveRecord::Base
 
   #validates_presence_of :date_of_birth
 
-  validates :current_address, presence: true #, format: { with: CITY_REGEX, message: "should alphabets only"}
+  validates :current_address, presence: true, format: { with: CITY_REGEX, message: "should alphabets only"}
 
-  validates :permanent_address, presence: true#, format: { with: CITY_REGEX, message: "should alphabets only"}
+  validates :permanent_address, presence: true, format: { with: CITY_REGEX, message: "should alphabets only"}
 
   validates :country, presence: true
 
-  validates :city, presence: true #, format: { with: CITY_REGEX, message: "should be alphabets only"}
+  validates :city, presence: true, format: { with: CITY_REGEX, message: "should be alphabets only"}
 
-  validates :course, presence: true#, format: { with: CITY_REGEX, message: "should be alphabets only"}
+  validates :course, presence: true, format: { with: CITY_REGEX, message: "should be alphabets only"}
 
-  validates :university, presence: true#, format: { with: CITY_REGEX, message: "should be alphabets only"}
+  validates :university, presence: true, format: { with: CITY_REGEX, message: "should be alphabets only"}
 
   validates :source, :presence => { :message => " - Please tick how did you get to know about KSC" }
 
