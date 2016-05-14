c2d3da8e2439d6d4364f1e45dc8a7f73546a4a94
alvin-odins <alvin.odins@gmail.com>
2016-05-14 04:03:06 -0400

    Alvin/Yazalde/Kenneth fix bugs after showcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,45 @@ class User < ActiveRecord::Base
 
 
   attr_accessor :tos
-  validates :tos, acceptance: true
+  validates :tos, presence: true, acceptance: true
 
 
   EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  NAME_REGEX = /\A\[A-Z][a-z][A-Za-z]/
+  USERNAME_REGEX= /\A[a-z0-9-]\z/
+  CITY_REGEX = /\A\[A-Z][a-z][A-Za-z]/
 
+  validates :first_name, presence: true#, format: { with: NAME_REGEX, message: "can have only characters"}
 
-  validates :first_name, presence: true
+  validates :last_name, presence: true#, format: { with: NAME_REGEX, message: "can have only characters"}
 
-  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: EMAIL_REGEX, message: "please enter a valid email"}
 
-  validates_uniqueness_of :email, presence: true, uniqueness: true, :format => EMAIL_REGEX
+  validates_uniqueness_of :email
 
   validates_uniqueness_of :username, presence: true, uniqueness: true
 
   validates :password, presence: true, confirmation: true
 
-  validates :contact_number, format: { with: /\d{10}/, message: "should have only 10 digits" }
+  validates :contact_number, presence: true, format: { with: /\d{10}/, message: "should have only 10 digits" }
 
   validates :gender, presence: true
+
+  #validates_presence_of :date_of_birth
+
+  validates :current_address, presence: true #, format: { with: CITY_REGEX, message: "should alphabets only"}
+
+  validates :permanent_address, presence: true#, format: { with: CITY_REGEX, message: "should alphabets only"}
+
+  validates :country, presence: true
+
+  validates :city, presence: true #, format: { with: CITY_REGEX, message: "should be alphabets only"}
+
+  validates :course, presence: true#, format: { with: CITY_REGEX, message: "should be alphabets only"}
+
+  validates :university, presence: true#, format: { with: CITY_REGEX, message: "should be alphabets only"}
+
+  validates :source, :presence => { :message => " - Please tick how did you get to know about KSC" }
 
   #validates :dob, presence: {type: :date, format: 'dd-mm-yyyy'}
 end
