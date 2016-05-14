afc1023107a6569044222a83ea4325887c097c0e
Abdulsattar Mohammed <asattar.md@gmail.com>
2016-05-14 17:34:07 +0530

    Add other source

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,12 +2,11 @@ $(function() {
   $('#copy_address').change(function() {
     $('#user_permanent_address').val($('#user_current_address').val());
   });
-  $('input[name="user[source]"]').change(function() {
-    if($('input[name="user[source]"]:checked').val() === 'Other') {
-      $('#user_source').show();
+  $('#user_source_other').change(function() {
+    if($(this).is(':checked')) {
+      $('#other_text').show();
     } else {
-      $('#user_source').hide();
+      $('#other_text').hide();
     }
-  });
-  $('input[name="user[source]"]').change();
+  }).change();
 });
