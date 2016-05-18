35774dfb4621f10d3179d290ca23f1ecd961ca25
alvin-odins <alvin.odins@gmail.com>
2016-05-18 18:18:44 -0400

    Alvin/Yazalde add dashboard_path to users controller, display text to dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     #if @user.save
     if verify_recaptcha(model: @user) && @user.save
       login @user.id
-      redirect_to user_path(@user)
+      redirect_to dashboard_path(@user)
     else
       flash[:danger] = 'You have not filled your details.'
       render :new
